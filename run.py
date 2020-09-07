584bb7c797f931097ed51eeeea3bff06cfdd167c
priscillapepe <priscillaungai99@gmail.com>
2020-09-07 13:37:44 +0300

    added more info

--- run.py
+++ run.py
@@ -7,4 +7,12 @@ def main():
         print("Welcome to pasword locker")
         print("\n")
         print("Select a short code to navigate through:to login 'lg',to exit 'ex':to create a new user 'nu'")
-        
+        short_code " input(),lower()
+        print("\n")
+
+    if short_code "" 'nu':
+        print("Create username")
+        created_user_name = input()
+
+        print("Create password")
+
