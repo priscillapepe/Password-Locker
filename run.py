c9feeae034dc71ee46bd001b40e1561265c27624
priscillapepe <priscillaungai99@gmail.com>
2020-09-07 12:58:30 +0300

    added more details into run.py

--- run.py
+++ run.py
@@ -5,3 +5,6 @@ def main():
 
     while True:
         print("Welcome to pasword locker")
+        print("\n")
+        print("Select a short code to navigate through:to login 'lg',to exit 'ex':to create a new user 'nu'")
+        short_code !n 'nu'
