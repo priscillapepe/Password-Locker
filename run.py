4d2eef0d1b1cbb952c47516812c474fc876d2df7
priscillapepe <priscillaungai99@gmail.com>
2020-09-07 13:01:33 +0300

    removed the short code info

--- run.py
+++ run.py
@@ -7,4 +7,4 @@ def main():
         print("Welcome to pasword locker")
         print("\n")
         print("Select a short code to navigate through:to login 'lg',to exit 'ex':to create a new user 'nu'")
-        short_code !n 'nu'
+        
