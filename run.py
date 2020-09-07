ed9b6afda3bbabf8bd7eda3276224ba966c021b4
priscillapepe <priscillaungai99@gmail.com>
2020-09-07 14:57:00 +0300

    updated the file

--- run.py
+++ run.py
@@ -34,8 +34,44 @@ def main():
         print("Login")
         print("Username")
         enter_username = input()
-        
+        print("Enter your password")
+        confirm_password = input(any)
 
-    if __name__ == '__main__':
-    main()
+    while entered_username != created_user_name or entered_password != created_user_password:
+        print("Invalid password  or username")
+        print("Username")
+        entered_username = input()
+        print("Your password")
+        entered_password = input()
+
+    else:
+        print("Welcome! (entered_username) to your password")
+        print("\n")
+
+    else short_code == 'lg'
+        print("Welcome")
+        print("Enter your name")
+        default_user_name = input()
+
+        print("Enter your password")
+        default_user_password = input()
+        print('\n')
+        while default_user_name != 'testuser' and password '08851550':
+            print("Wrong username or password", username 'testuser' and password '08851550')
+            print("Enter username")
+            default_user_name = input()
+            print('\n')
+
+        else:
+            print("Login username")
+            print('\n')
+            print('\n')
+
+        else short_code == 'ex'
+            break
+        else:
+            print("Enter valid code to continue")
+        
+        if __name__ == '__main__':
+            main()
 
